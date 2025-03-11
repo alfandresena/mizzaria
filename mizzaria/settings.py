3653733b7799ee116c16c4d6d48150c6f9168a8f
sahyan <tahirihasinarakotomanga@gmail.com>
2025-03-11 23:34:57 +0300

    fusion et verification des conflits

--- mizzaria/settings.py
+++ mizzaria/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,12 +42,13 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5k=@$&2jj+40qsb6_)h1px66ttdfjxs*j*rzzqre@n^1@=^rh_'
+# SECRET_KEY = 'django-insecure-5k=@$&2jj+40qsb6_)h1px66ttdfjxs*j*rzzqre@n^1@=^rh_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -97,11 +100,15 @@ WSGI_APPLICATION = 'mizzaria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
