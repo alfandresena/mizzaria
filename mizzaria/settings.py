0682000ece980922a80f6c5ff3c2299a95436b72
sahyan <tahirihasinarakotomanga@gmail.com>
2025-03-12 06:18:18 +0300

    feat: .env

--- mizzaria/settings.py
+++ mizzaria/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 
 # if we wanna use the server onrender : 
 from decouple import config
-import dj_database_url
+# import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
